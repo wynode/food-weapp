69c368dc3abea552cdfdd91cc5f3554f8b35ebce
wynode <bugeikela@gmail.com>
2023-08-19 00:10:51 +0800

    feat: 添加报告详情页面

---
+++ pages/report-profile/index.js
@@ -0,0 +1,51 @@
+// pages/report-profile/index.js
+Page({
+
+
+  data: {
+    reportList: [{
+        date: '7.11',
+        dateType: '日报',
+        total: 5,
+        unqualifiedTotal: 0,
+        submitTime: '09:00',
+        submitUser: '花花',
+        submitType: '门店提交',
+        address: '成都市金牛区驷马桥街道横田大厦'
+      },
+      {
+        date: '7.10',
+        dateType: '日报',
+        total: 10,
+        unqualifiedTotal: 3,
+        submitTime: '09:00',
+        submitUser: '花花',
+        submitType: '门店提交',
+        address: '成都市金牛区驷马桥街道横田大厦'
+      },
+      {
+        date: '7.09',
+        dateType: '日报',
+        total: 5,
+        unqualifiedTotal: 0,
+        submitTime: '09:00',
+        submitUser: '花花',
+        submitType: '门店提交',
+        address: '成都市金牛区驷马桥街道横田大厦'
+      },
+      {
+        date: '7.08',
+        dateType: '日报',
+        total: 3,
+        unqualifiedTotal: 2,
+        submitTime: '09:00',
+        submitUser: '花花',
+        submitType: '门店提交',
+        address: '成都市金牛区驷马桥街道横田大厦'
+      },
+
+    ]
+  },
+
+
+})
